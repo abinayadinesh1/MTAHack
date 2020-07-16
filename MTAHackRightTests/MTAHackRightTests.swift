ef603af74b4b740baecdadb2b3a6ee0f2f9a915c
Abinaya Dinesh <abinayadinesh1@gmail.com>
2020-07-16 16:20:49 -0400

    Initial Commit

---
+++ MTAHackRightTests/MTAHackRightTests.swift
@@ -0,0 +1,34 @@
+//
+//  MTAHackRightTests.swift
+//  MTAHackRightTests
+//
+//  Created by Abinaya Dinesh on 7/16/20.
+//  Copyright © 2020 Abinaya Dinesh. All rights reserved.
+//
+
+import XCTest
+@testable import MTAHackRight
+
+class MTAHackRightTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
